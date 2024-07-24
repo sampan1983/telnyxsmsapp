9785d2aee5ec5748b01cb8c13aeb3decc40eee7e
sampan saini <sampan1983@gmail.com>
2024-07-24 19:13:17 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "vm@321";
-$db="app_democalling";
+$servername = "q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "q40safy2jq1aej56";
+$password = "frbhki4aokob1hzv";
+$db="dz1oduatpy1ct2mo";
 
 // Create connection
 $con = new mysqli($servername, $username, $password,$db);
