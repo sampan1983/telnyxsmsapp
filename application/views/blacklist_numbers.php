362019eb34c834a832106f96accbea9715e6e4ca
sampan saini <sampan1983@gmail.com>
2024-09-17 17:30:47 +0530

    Update blacklist_numbers.php

--- application/views/blacklist_numbers.php
+++ application/views/blacklist_numbers.php
@@ -1,11 +1,4 @@
 <?php include_once('header.php') ?>
-
-<!-- DataTables CSS -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-<!-- DataTables Buttons CSS -->
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
-
-
     <main class="main">
 
 
@@ -61,6 +54,10 @@
 
 <!-- Copy button -->
 <button id="copyBlacklist" class="btn btn-primary">Copy All Blacklist Numbers</button>
+
+<a href="Clients/export2"><button type="button" value="Export Data" class="btn btn-primary btn-md btn-sty sky-color"><i class="fa fa-file-excel-o" aria-hidden="true"></i>  Export Data</button></a>
+
+
 
               </div>
 
@@ -260,78 +257,7 @@
 
 
 
-<!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<!-- DataTables JS -->
-<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<!-- DataTables Buttons JS -->
-<script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
-<!-- JSZip for Excel export -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<!-- DataTables HTML5 export JS -->
-<script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
-<!-- DataTables Print export JS -->
-<script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
-
-
-
 <script>
-$(document).ready(function() {
-    // Initialize DataTable with export buttons
-    var table = $('#dataTable').DataTable({
-        paging: true, // Enable pagination
-        dom: 'Bfrtip', // Add export buttons layout
-        buttons: [
-            {
-                extend: 'copy', // Default copy button from DataTables
-                text: 'Copy All'
-            },
-            {
-                extend: 'csv', // Export to CSV
-                text: 'Export CSV'
-            },
-            {
-                extend: 'excel', // Export to Excel
-                text: 'Export Excel'
-            },
-            {
-                extend: 'pdf', // Export to PDF
-                text: 'Export PDF'
-            },
-            {
-                extend: 'print', // Print option
-                text: 'Print'
-            }
-        ]
-    });
-
-    // Custom copy blacklist button logic
-    $('#copyBlacklist').on('click', function() {
-        // Get only the "Number" column (second column)
-        var blacklistNumbers = [];
-        table.rows().every(function() {
-            var data = this.data();
-            blacklistNumbers.push(data[1]); // Get the second column (Number)
-        });
-
-        // Copy to clipboard
-        var tempElement = $('<textarea>');
-        tempElement.val(blacklistNumbers.join('\n')).appendTo('body').select();
-        document.execCommand('copy');
-        tempElement.remove();
-
-        alert('Blacklist numbers copied to clipboard!');
-    });
-});
-</script>
-
-
-
-
-
-
-
-<!-- <script>
 $(document).ready(function() {
     // Initialize DataTable without pagination
     var table = $('#dataTable').DataTable({
@@ -394,7 +320,7 @@ $(document).ready(function() {
 
 
 
-  </script> -->
+  </script>
 
 
 
